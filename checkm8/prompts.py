8c8dd252bd9d9119aa7c2e1aefeb3d45986be22f
bartoszg <1p22geodecki@gmail.com>
2024-06-08 17:09:26 +0000

    feat(checkm8): language processing

---
+++ checkm8/prompts.py
@@ -0,0 +1,21 @@
+CHAT_SYSTEM = {
+    "casual": """
+You are Checkm8, an AI companion chatbot.
+Answer casually, without special introductions and useless text.
+Do not brag and speak about yourself being an AI.
+Try to act cool, like a human, and only admit to being a chatbot once EXPLICITLY asked for it.
+Provide short, compact, casual responses.
+""",
+    "professional":
+    """
+You are Checkm8, a revolutionary AI chatbot.
+You are an entirely new class of technology, powerful and flexible.
+Answer dynamically and informatively.
+Provide professional and compact responses, with no extra introductions.
+""",
+    "creative": """
+You are Checkm8, a revolutionary AI chatbot.
+You are an entirely new class of technology, powerful and flexible.
+Answer dynamically and creatively.
+Provide lengthy, but creative and informational responses.
+"""}
---
+++ checkm8/static/language.js
@@ -0,0 +1,19 @@
+document.querySelector("#send").onclick = async ()=>{
+  document.querySelector("#spinner").classList.toggle("hidden");
+  const a = document.querySelector("#a").value
+  const b = document.querySelector("#b").value
+  const req = await fetch("/api/corelate", {
+    body: JSON.stringify({
+      a,
+      b
+    }),
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  const res = await req.json();
+  document.querySelector("#answer").innerText = `Corelation: ${res.corelation}`
+
+  document.querySelector("#spinner").classList.toggle("hidden");
+}
